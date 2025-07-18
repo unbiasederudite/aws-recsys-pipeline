4e72023933ed69f72f95faa66e301c3655d23a76
Mykhailo Stasiuk <erudite1854@gmail.com>
2025-07-18 21:25:53 +0200

    Added initial VPC module

---
+++ terraform/modules/vpc/variables.tf
@@ -0,0 +1,9 @@
+variable "cidr_block" {
+  description = "CIDR block for the VPC resource"
+  type        = string
+}
+
+variable "name" {
+  description = "Name for the VPC resource"
+  type        = string
+}
